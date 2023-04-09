802f1615b8c3f26d67a32a03584101d629a72eb0
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-04-09 17:48:24 +0530

    Added main files

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ quiz_gui = Quizller(quiz)
 # while quiz.still_has_questions():
 #     quiz.next_question()
 
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+# print("You've completed the quiz")
+# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
